006ef66885a4bdd3c372b6c8644cd0431b6677eb
asterox <62700841+roxov@users.noreply.github.com>
2020-03-29 17:39:00 +0200

    Premiers essais variables

---
+++ src/fr/asterox/grandsdebuts/core/Starter.java
@@ -0,0 +1,20 @@
+package fr.asterox.grandsdebuts.core;
+
+public class Starter {
+	public static void main (String... args) {
+		String text = "Je veux savoir combien j'ai de voyelles ici" ;
+		int numberOfVowels = 0 ;
+		double percentageOfVowels = 0.0 ;
+		System.out.println("Il y a " + numberOfVowels + " voyelles dans ce texte, soit" + percentageOfVowels + "%" ) ;
+		// Définition de variables.
+		
+		final int numberOfPets = 1 ;
+		// Constante non modifiable
+		
+		
+		int a = 10 ;
+		int b = 4 ;
+		double c = a / b ;
+		// Si un calcul est fait à partir de variables de type entier, le résultat sera un entier.Voir plus dans Calcul => combinaisons de variables.
+	}
+}
